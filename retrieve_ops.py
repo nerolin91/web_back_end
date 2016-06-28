0dcdb7fb53402c8250c8214c9e23c20c0b7eefac
jasdeep-jassal <jjassal@sfu.ca>
2016-06-27 21:55:50 -0700

    Add retrieval of user by id

--- retrieve_ops.py
+++ retrieve_ops.py
@@ -1,8 +1,44 @@
 ''' Retrieve operations for CMPT 474 Assignment 2 '''
 
-def retrieve_by_id(table, id, response):
+from boto.dynamodb2.items import Item
+from boto.dynamodb2.exceptions import ItemNotFound
+
+def retrieve_by_id_old(table, id, response):
     print "Retrieve by id not yet implemented"
+
     response.status = 501
+    
     return {"errors": [{
-        "retrieve by id not implemented": {"id": id}
+        "retrieve by id not yet 1implemented": {"id": id}
         }]}
+
+def retrieve_by_id(table, id, response):
+    try:
+        item = table.get_item(id=id)
+        name = item["name"]
+        activities = item["activities"]
+
+        if activities is None:
+            activities = []
+
+        response.status = 200 
+        return  {
+                    "data": {
+                        "type": "person",
+                        "id": id,
+                        "name": name,
+                        "activities": activities
+                    }
+                }
+
+    except ItemNotFound as inf:
+        print "Error: not found"
+
+        response.status = 404 # "Not Found"
+        return  {
+                    "errors": [{
+                        "not_found": {
+                            "id": id
+                        }
+                    }]
+                }
